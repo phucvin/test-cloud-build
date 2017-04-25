e2ab5c248139aa81e21fe199f0a94145aa5e7200
phucvin <phucvin@gmail.com>
2017-04-25 23:53:54 +0700

    pre and post build

---
+++ uproj/Assets/Editor/AdditionalCloudBuild.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public static class AdditionalCloudBuild
+{
+    [MenuItem("ACB/PreExport")]
+    public static void PreExport()
+    {
+        Debug.Log("ACB.PreExport");
+
+        Directory.CreateDirectory(Application.dataPath + "/StreamingAssets/dlc/");
+        File.Copy(Application.dataPath + "/../../dlc/data.txt",
+            Application.dataPath + "/StreamingAssets/dlc/data.txt", overwrite: true);
+    }
+
+    [MenuItem("ACB/PostExport")]
+    public static void PostExport()
+    {
+        Debug.Log("ACB.PreExport");
+    }
+}
